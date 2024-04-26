b4c0357bd36c879e11ce71651e7e005309055e3b
Zandra Maria De Almeida Vieitez <z.de.almeida.vieitez@avanade.com>
2024-04-26 17:44:00 -0300

    ContoleCursoVersao01

---
+++ ControleCursoImpacta/Controllers/AlunoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleCursoImpacta.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Aluno()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControleCursoImpacta/Controllers/CursoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleCursoImpacta.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Curso()
+        {
+            return View();
+        }
+    }
+}
